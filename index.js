7e858595dc0fd1a5478b40a3a6a8fb51acf7e560
CoderZIE <102377644+CoderZIE@users.noreply.github.com>
2022-10-04 10:28:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,193 @@
+//Zaqi''s Javascript
+
+$( function() {
+  $( "#slider" ).slider();
+} );
+
+// Javascript for slider
+
+window.onscroll = function(event){
+  var scr = window.pageYOffset;
+  document.getElementsByClassName('zslider')[0].value= scr;
+  document.getElementsByClassName('zslider')[1].value= (scr/3);
+  document.getElementsByClassName('zslider')[2].value= (scr/5);
+  if(src>100 && src<200){
+    document.getElementsByClassName('zslider')[0].value= (100-scr);
+  }
+  if(src>300 && src<600){
+    document.getElementsByClassName('zslider')[1].value= (200-(scr/3));
+  }
+}
+
+// javascript for explore elements
+
+$(document).ready(function(){
+  $(".zbr").mouseenter(function(){
+    $(".e1border").css("width", "0");
+    $(".e2border").css("width", "112px");
+    $(".e3border").css("width", "0");
+    $(".e4border").css("width", "0");
+    $(".e5border").css("width", "0");
+    $(".e6border").css("width", "112px");
+    $(".e7border").css("width", "0");
+    $(".e8border").css("width", "0");
+    $(".e9border").css("width", "0");
+    $(".e10border").css("width", "0");
+    $(".e11border").css("width", "0");
+    $(".e12border").css("width", "0");
+    $(".e13border").css("width", "0");
+  });
+  $(".zbr").mouseleave(function(){
+        $(".e1border").css("width", "30px");
+        $(".e2border").css("width", "30px");
+        $(".e3border").css("width", "50px");
+        $(".e4border").css("width", "10px");
+        $(".e5border").css("width", "10px");
+        $(".e6border").css("width", "20px");
+        $(".e7border").css("width", "10px");
+        $(".e8border").css("width", "10px");
+        $(".e9border").css("width", "30px");
+        $(".e10border").css("width", "30px");
+        $(".e11border").css("width", "30px");
+        $(".e12border").css("width", "10px");
+        $(".e13border").css("width", "10px");
+   });
+});
+
+$(document).ready(function(){
+  $(".zbr2").mouseenter(function(){
+    $(".s1border").css("width", "0");
+    $(".s2border").css("width", "112px");
+    $(".s3border").css("width", "0");
+    $(".s4border").css("width", "0");
+    $(".s5border").css("width", "0");
+    $(".s6border").css("width", "112px");
+    $(".s7border").css("width", "0");
+    $(".s8border").css("width", "0");
+    $(".s9border").css("width", "0");
+    $(".s10border").css("width", "0");
+    $(".s11border").css("width", "0");
+    $(".s12border").css("width", "0");
+    $(".s13border").css("width", "0");
+  });
+  $(".zbr2").mouseleave(function(){
+        $(".s1border").css("width", "30px");
+        $(".s2border").css("width", "30px");
+        $(".s3border").css("width", "50px");
+        $(".s4border").css("width", "10px");
+        $(".s5border").css("width", "10px");
+        $(".s6border").css("width", "20px");
+        $(".s7border").css("width", "10px");
+        $(".s8border").css("width", "10px");
+        $(".s9border").css("width", "30px");
+        $(".s10border").css("width", "30px");
+        $(".s11border").css("width", "30px");
+        $(".s12border").css("width", "10px");
+        $(".s13border").css("width", "10px");
+   });
+
+   
+});
+
+$(document).ready(function(){
+  $(".zbr3").mouseenter(function(){
+    $(".t1border").css("width", "0");
+    $(".t2border").css("width", "112px");
+    $(".t3border").css("width", "0");
+    $(".t4border").css("width", "0");
+    $(".t5border").css("width", "0");
+    $(".t6border").css("width", "112px");
+    $(".t7border").css("width", "0");
+    $(".t8border").css("width", "0");
+    $(".t9border").css("width", "0");
+    $(".t10border").css("width", "0");
+    $(".t11border").css("width", "0");
+    $(".t12border").css("width", "0");
+    $(".t13border").css("width", "0");
+  });
+  $(".zbr3").mouseleave(function(){
+        $(".t1border").css("width", "30px");
+        $(".t2border").css("width", "30px");
+        $(".t3border").css("width", "50px");
+        $(".t4border").css("width", "10px");
+        $(".t5border").css("width", "10px");
+        $(".t6border").css("width", "20px");
+        $(".t7border").css("width", "10px");
+        $(".t8border").css("width", "10px");
+        $(".t9border").css("width", "30px");
+        $(".t10border").css("width", "30px");
+        $(".t11border").css("width", "30px");
+        $(".t12border").css("width", "10px");
+        $(".t13border").css("width", "10px");
+   });
+});
+
+$(document).ready(function(){
+  $(".zbr4").mouseenter(function(){
+    $(".y1border").css("width", "0");
+    $(".y2border").css("width", "112px");
+    $(".y3border").css("width", "0");
+    $(".y4border").css("width", "0");
+    $(".y5border").css("width", "0");
+    $(".y6border").css("width", "112px");
+    $(".y7border").css("width", "0");
+    $(".y8border").css("width", "0");
+    $(".y9border").css("width", "0");
+    $(".y10border").css("width", "0");
+    $(".y11border").css("width", "0");
+    $(".y12border").css("width", "0");
+    $(".y13border").css("width", "0");
+  });
+  $(".zbr4").mouseleave(function(){
+        $(".y1border").css("width", "30px");
+        $(".y2border").css("width", "30px");
+        $(".y3border").css("width", "50px");
+        $(".y4border").css("width", "10px");
+        $(".y5border").css("width", "10px");
+        $(".y6border").css("width", "20px");
+        $(".y7border").css("width", "10px");
+        $(".y8border").css("width", "10px");
+        $(".y9border").css("width", "30px");
+        $(".y10border").css("width", "30px");
+        $(".y11border").css("width", "30px");
+        $(".y12border").css("width", "10px");
+        $(".y13border").css("width", "10px");
+   });
+});
+
+$(document).ready(function(){
+  $(".zbr5").mouseenter(function(){
+    $(".c1border").css("width", "0");
+    $(".c2border").css("width", "112px");
+    $(".c3border").css("width", "0");
+    $(".c4border").css("width", "0");
+    $(".c5border").css("width", "0");
+    $(".c6border").css("width", "112px");
+    $(".c7border").css("width", "0");
+    $(".c8border").css("width", "0");
+    $(".c9border").css("width", "0");
+    $(".c10border").css("width", "0");
+    $(".c11border").css("width", "0");
+    $(".c12border").css("width", "0");
+    $(".c13border").css("width", "0");
+  });
+  $(".zbr5").mouseleave(function(){
+        $(".c1border").css("width", "30px");
+        $(".c2border").css("width", "30px");
+        $(".c3border").css("width", "50px");
+        $(".c4border").css("width", "10px");
+        $(".c5border").css("width", "10px");
+        $(".c6border").css("width", "20px");
+        $(".c7border").css("width", "10px");
+        $(".c8border").css("width", "10px");
+        $(".c9border").css("width", "30px");
+        $(".c10border").css("width", "30px");
+        $(".c11border").css("width", "30px");
+        $(".c12border").css("width", "10px");
+        $(".c13border").css("width", "10px");
+   });
+});
+
 
 $( function() {
   $( "#slider" ).slider();
