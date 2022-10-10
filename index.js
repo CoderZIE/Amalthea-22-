f083b98d07b5da959a1a332b0d7fb306931163ca
Rudra Vengurlekar <107509068+QuantRV@users.noreply.github.com>
2022-10-11 01:15:10 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,110 +1,110 @@
 //Zaqi''s Javascript
 
-$( function() {
-  $( "#slider" ).slider();
-} );
+$(function () {
+  $("#slider").slider();
+});
 
 // Javascript for slider
 
 //clapping the function
 
-window.onscroll = function(event){
+window.onscroll = function (event) {
   var scr = window.pageYOffset;
-// for the first slider 
-  
-var c= scr/3;
-var a= c%100;
-var b= Math.floor(c/100); 
+  // for the first slider 
 
-  if(c<100){
-    document.getElementsByClassName('zslider')[0].value= (c);
+  var c = scr / 3;
+  var a = c % 100;
+  var b = Math.floor(c / 100);
+
+  if (c < 100) {
+    document.getElementsByClassName('zslider')[0].value = (c);
   }
 
-  else{
-    if(b%2==0){
-      document.getElementsByClassName('zslider')[0].value= (a);
+  else {
+    if (b % 2 == 0) {
+      document.getElementsByClassName('zslider')[0].value = (a);
     }
-    else{
-      document.getElementsByClassName('zslider')[0].value= (100-a);
+    else {
+      document.getElementsByClassName('zslider')[0].value = (100 - a);
     }
   }
 
-// for the second slider
-var d= scr/5;
-a= d%100;
-b= Math.floor(d/100); 
-if(d<100){
-  document.getElementsByClassName('zslider')[1].value= (d);
-}
-
-else{
-  if(b%2==0){
-    document.getElementsByClassName('zslider')[1].value= (a);
+  // for the second slider
+  var d = scr / 5;
+  a = d % 100;
+  b = Math.floor(d / 100);
+  if (d < 100) {
+    document.getElementsByClassName('zslider')[1].value = (d);
   }
-  else{
-    document.getElementsByClassName('zslider')[1].value= (100-a);
+
+  else {
+    if (b % 2 == 0) {
+      document.getElementsByClassName('zslider')[1].value = (a);
+    }
+    else {
+      document.getElementsByClassName('zslider')[1].value = (100 - a);
+    }
   }
-}
 
-// for the third slider
-var e= scr/7;
-a= e%100;
-b= Math.floor(e/100); 
-if(e<100){
-  document.getElementsByClassName('zslider')[2].value= (e);
-}
-
-else{
-  if(b%2==0){
-    document.getElementsByClassName('zslider')[2].value= (a);
+  // for the third slider
+  var e = scr / 7;
+  a = e % 100;
+  b = Math.floor(e / 100);
+  if (e < 100) {
+    document.getElementsByClassName('zslider')[2].value = (e);
   }
-  else{
-    document.getElementsByClassName('zslider')[2].value= (100-a);
+
+  else {
+    if (b % 2 == 0) {
+      document.getElementsByClassName('zslider')[2].value = (a);
+    }
+    else {
+      document.getElementsByClassName('zslider')[2].value = (100 - a);
+    }
   }
-}
-
-  
 
 
-// for the fourth slider
-var f= scr/10;
-a= f%100;
-b= Math.floor(f/100); 
-if(f<100){
-  document.getElementsByClassName('zslider')[3].value= (f);
-}
 
-else{
-  if(b%2==0){
-    document.getElementsByClassName('zslider')[3].value= (a);
+
+  // for the fourth slider
+  var f = scr / 10;
+  a = f % 100;
+  b = Math.floor(f / 100);
+  if (f < 100) {
+    document.getElementsByClassName('zslider')[3].value = (f);
   }
-  else{
-    document.getElementsByClassName('zslider')[3].value= (100-a);
-  }
-}
 
-//for the fifth slider
-var g= scr/13;
-a= g%100;
-b= Math.floor(g/100); 
-if(g<100){
-  document.getElementsByClassName('zslider')[4].value= (g);
-}
+  else {
+    if (b % 2 == 0) {
+      document.getElementsByClassName('zslider')[3].value = (a);
+    }
+    else {
+      document.getElementsByClassName('zslider')[3].value = (100 - a);
+    }
+  }
 
-else{
-  if(b%2==0){
-    document.getElementsByClassName('zslider')[4].value= (a);
+  //for the fifth slider
+  var g = scr / 13;
+  a = g % 100;
+  b = Math.floor(g / 100);
+  if (g < 100) {
+    document.getElementsByClassName('zslider')[4].value = (g);
   }
-  else{
-    document.getElementsByClassName('zslider')[4].value= (100-a);
+
+  else {
+    if (b % 2 == 0) {
+      document.getElementsByClassName('zslider')[4].value = (a);
+    }
+    else {
+      document.getElementsByClassName('zslider')[4].value = (100 - a);
+    }
   }
-}
 
 }
 // javascript for explore elements
 
-$(document).ready(function(){
-  $(".zbr").mouseenter(function(){
+$(document).ready(function () {
+  $(".zbr").mouseenter(function () {
     $(".e1border").css("width", "0");
     $(".e2border").css("width", "112px");
     $(".e3border").css("width", "0");
@@ -119,25 +119,25 @@ $(document).ready(function(){
     $(".e12border").css("width", "0");
     $(".e13border").css("width", "0");
   });
-  $(".zbr").mouseleave(function(){
-        $(".e1border").css("width", "30px");
-        $(".e2border").css("width", "30px");
-        $(".e3border").css("width", "50px");
-        $(".e4border").css("width", "10px");
-        $(".e5border").css("width", "10px");
-        $(".e6border").css("width", "20px");
-        $(".e7border").css("width", "10px");
-        $(".e8border").css("width", "10px");
-        $(".e9border").css("width", "30px");
-        $(".e10border").css("width", "30px");
-        $(".e11border").css("width", "30px");
-        $(".e12border").css("width", "10px");
-        $(".e13border").css("width", "10px");
-   });
+  $(".zbr").mouseleave(function () {
+    $(".e1border").css("width", "30px");
+    $(".e2border").css("width", "30px");
+    $(".e3border").css("width", "50px");
+    $(".e4border").css("width", "10px");
+    $(".e5border").css("width", "10px");
+    $(".e6border").css("width", "20px");
+    $(".e7border").css("width", "10px");
+    $(".e8border").css("width", "10px");
+    $(".e9border").css("width", "30px");
+    $(".e10border").css("width", "30px");
+    $(".e11border").css("width", "30px");
+    $(".e12border").css("width", "10px");
+    $(".e13border").css("width", "10px");
+  });
 });
 
-$(document).ready(function(){
-  $(".zbr2").mouseenter(function(){
+$(document).ready(function () {
+  $(".zbr2").mouseenter(function () {
     $(".s1border").css("width", "0");
     $(".s2border").css("width", "112px");
     $(".s3border").css("width", "0");
@@ -152,27 +152,27 @@ $(document).ready(function(){
     $(".s12border").css("width", "0");
     $(".s13border").css("width", "0");
   });
-  $(".zbr2").mouseleave(function(){
-        $(".s1border").css("width", "30px");
-        $(".s2border").css("width", "30px");
-        $(".s3border").css("width", "50px");
-        $(".s4border").css("width", "10px");
-        $(".s5border").css("width", "10px");
-        $(".s6border").css("width", "20px");
-        $(".s7border").css("width", "10px");
-        $(".s8border").css("width", "10px");
-        $(".s9border").css("width", "30px");
-        $(".s10border").css("width", "30px");
-        $(".s11border").css("width", "30px");
-        $(".s12border").css("width", "10px");
-        $(".s13border").css("width", "10px");
-   });
+  $(".zbr2").mouseleave(function () {
+    $(".s1border").css("width", "30px");
+    $(".s2border").css("width", "30px");
+    $(".s3border").css("width", "50px");
+    $(".s4border").css("width", "10px");
+    $(".s5border").css("width", "10px");
+    $(".s6border").css("width", "20px");
+    $(".s7border").css("width", "10px");
+    $(".s8border").css("width", "10px");
+    $(".s9border").css("width", "30px");
+    $(".s10border").css("width", "30px");
+    $(".s11border").css("width", "30px");
+    $(".s12border").css("width", "10px");
+    $(".s13border").css("width", "10px");
+  });
 
-   
+
 });
 
-$(document).ready(function(){
-  $(".zbr3").mouseenter(function(){
+$(document).ready(function () {
+  $(".zbr3").mouseenter(function () {
     $(".t1border").css("width", "0");
     $(".t2border").css("width", "112px");
     $(".t3border").css("width", "0");
@@ -187,25 +187,25 @@ $(document).ready(function(){
     $(".t12border").css("width", "0");
     $(".t13border").css("width", "0");
   });
-  $(".zbr3").mouseleave(function(){
-        $(".t1border").css("width", "30px");
-        $(".t2border").css("width", "30px");
-        $(".t3border").css("width", "50px");
-        $(".t4border").css("width", "10px");
-        $(".t5border").css("width", "10px");
-        $(".t6border").css("width", "20px");
-        $(".t7border").css("width", "10px");
-        $(".t8border").css("width", "10px");
-        $(".t9border").css("width", "30px");
-        $(".t10border").css("width", "30px");
-        $(".t11border").css("width", "30px");
-        $(".t12border").css("width", "10px");
-        $(".t13border").css("width", "10px");
-   });
+  $(".zbr3").mouseleave(function () {
+    $(".t1border").css("width", "30px");
+    $(".t2border").css("width", "30px");
+    $(".t3border").css("width", "50px");
+    $(".t4border").css("width", "10px");
+    $(".t5border").css("width", "10px");
+    $(".t6border").css("width", "20px");
+    $(".t7border").css("width", "10px");
+    $(".t8border").css("width", "10px");
+    $(".t9border").css("width", "30px");
+    $(".t10border").css("width", "30px");
+    $(".t11border").css("width", "30px");
+    $(".t12border").css("width", "10px");
+    $(".t13border").css("width", "10px");
+  });
 });
 
-$(document).ready(function(){
-  $(".zbr4").mouseenter(function(){
+$(document).ready(function () {
+  $(".zbr4").mouseenter(function () {
     $(".y1border").css("width", "0");
     $(".y2border").css("width", "112px");
     $(".y3border").css("width", "0");
@@ -220,25 +220,25 @@ $(document).ready(function(){
     $(".y12border").css("width", "0");
     $(".y13border").css("width", "0");
   });
-  $(".zbr4").mouseleave(function(){
-        $(".y1border").css("width", "30px");
-        $(".y2border").css("width", "30px");
-        $(".y3border").css("width", "50px");
-        $(".y4border").css("width", "10px");
-        $(".y5border").css("width", "10px");
-        $(".y6border").css("width", "20px");
-        $(".y7border").css("width", "10px");
-        $(".y8border").css("width", "10px");
-        $(".y9border").css("width", "30px");
-        $(".y10border").css("width", "30px");
-        $(".y11border").css("width", "30px");
-        $(".y12border").css("width", "10px");
-        $(".y13border").css("width", "10px");
-   });
+  $(".zbr4").mouseleave(function () {
+    $(".y1border").css("width", "30px");
+    $(".y2border").css("width", "30px");
+    $(".y3border").css("width", "50px");
+    $(".y4border").css("width", "10px");
+    $(".y5border").css("width", "10px");
+    $(".y6border").css("width", "20px");
+    $(".y7border").css("width", "10px");
+    $(".y8border").css("width", "10px");
+    $(".y9border").css("width", "30px");
+    $(".y10border").css("width", "30px");
+    $(".y11border").css("width", "30px");
+    $(".y12border").css("width", "10px");
+    $(".y13border").css("width", "10px");
+  });
 });
 
-$(document).ready(function(){
-  $(".zbr5").mouseenter(function(){
+$(document).ready(function () {
+  $(".zbr5").mouseenter(function () {
     $(".c1border").css("width", "0");
     $(".c2border").css("width", "112px");
     $(".c3border").css("width", "0");
@@ -253,93 +253,114 @@ $(document).ready(function(){
     $(".c12border").css("width", "0");
     $(".c13border").css("width", "0");
   });
-  $(".zbr5").mouseleave(function(){
-        $(".c1border").css("width", "30px");
-        $(".c2border").css("width", "30px");
-        $(".c3border").css("width", "50px");
-        $(".c4border").css("width", "10px");
-        $(".c5border").css("width", "10px");
-        $(".c6border").css("width", "20px");
-        $(".c7border").css("width", "10px");
-        $(".c8border").css("width", "10px");
-        $(".c9border").css("width", "30px");
-        $(".c10border").css("width", "30px");
-        $(".c11border").css("width", "30px");
-        $(".c12border").css("width", "10px");
-        $(".c13border").css("width", "10px");
-   });
+  $(".zbr5").mouseleave(function () {
+    $(".c1border").css("width", "30px");
+    $(".c2border").css("width", "30px");
+    $(".c3border").css("width", "50px");
+    $(".c4border").css("width", "10px");
+    $(".c5border").css("width", "10px");
+    $(".c6border").css("width", "20px");
+    $(".c7border").css("width", "10px");
+    $(".c8border").css("width", "10px");
+    $(".c9border").css("width", "30px");
+    $(".c10border").css("width", "30px");
+    $(".c11border").css("width", "30px");
+    $(".c12border").css("width", "10px");
+    $(".c13border").css("width", "10px");
+  });
 });
 
 
-$( function() {
-  $( "#slider" ).slider();
-} );
+$(function () {
+  $("#slider").slider();
+});
 
 
 // Rudra JS Start
 
- function preloaderFunction(){
-    document.querySelector(".Rpreload").style.display = "none"
+function preloaderFunction() {
+  document.querySelector(".Rpreload").style.display = "none"
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 document.addEventListener('DOMContentLoaded', function (event) {
 
-    var dataText = ["nitate", "nnovate", "nspire"];
-    // var i = 4
+  var dataText = ["nitatee", "nnovatee", "nspiree"];
+  // var i = 4
 
-    function displayCompleteText(a) {
-        document.getElementById("RiiiText").style.display = "None";
-        document.getElementById("Rrotate").style.animation = "rotation 12s infinite linear";
-        document.getElementById("Rrotate").style.animationPlayState = "running";
-        setTimeout(a, 3000);
+  function displayCompleteText(a) {
+    document.getElementById("RiiiText").style.display = "None";
+    document.getElementById("Rrotate").style.animation = "rotation 6.00s infinite linear";
+    document.getElementById("Rrotate").style.animationPlayState = "running";
+    setTimeout(a, 2000);
+  }
+
+
+  function typeWriter(text, i, fnCallback) {
+    // chekc if text isn't finished yet
+    if (i + 1 < (text.length)) {
+
+      // add next character to h1
+      document.getElementById("RiiiText").style.display = "Flex";
+      document.getElementById("Rrotate").style.removeProperty("animation");
+      document.getElementById("RiiiText").innerHTML = text.substring(0, i + 1) + '<span aria-hidden="true"></span>';
+
+      // wait for a while and call this function again for next character
+      setTimeout(function () {
+        typeWriter(text, i + 1, fnCallback)
+      }, 100)
     }
-
-
-    function typeWriter(text, i, fnCallback) {
-        // chekc if text isn't finished yet
-        if (i < (text.length)) {
-            // add next character to h1
-
-            document.getElementById("RiiiText").style.display = "Flex";
-            document.getElementById("Rrotate").style.removeProperty("animation");
-           
-
-            document.getElementById("RiiiText").innerHTML = text.substring(0, i + 1) + '<span aria-hidden="true"></span>';
-
-            // wait for a while and call this function again for next character
+    else if (i + 1 === (text.length)) {
+      async function demo(text,i,fnCallback) {
+        await sleep(1600); // full text display 
+        for (let m = text.length - 1; m > -2; m--) {
+          if (m != 0) {
+            // console.log(text.substring(0, m) + '<span aria-hidden="true"></span>')
+            document.getElementById("RiiiText").innerHTML = text.substring(0, m) + '<span aria-hidden="true"></span>';
             setTimeout(function () {
-                typeWriter(text, i + 1, fnCallback)
-            }, 100)
+              // console.log();
+            }, 0)
+            await sleep(70); //erase speed
+          }
+          else {
+            typeWriter(text, i + 2, fnCallback)
+          }
         }
-
-        // text finished, call callback if there is a callback function
-        else if (typeof fnCallback == 'function') {
-            setTimeout(function () {
-                displayCompleteText(fnCallback)
-            }, 1000)
-        }
+        console.log('Done');
+      }
+      demo(text,i,fnCallback)
     }
-    // start a typewriter animation for a text in the dataText array
-    function StartTextAnimation(i) {
-        if (typeof dataText[i] == 'undefined') {
-            setTimeout(function () {
-                StartTextAnimation(0);
-            }, 0);
-        }
-        // check if dataText[i] exists
-        if (i < dataText[i].length) {
-            // text exists! start typewriter animation
-            typeWriter(dataText[i], 0, function () {
-                document.getElementById("Rrotate").style.removeProperty("animation");
-                document.getElementById("Rrotate").style.transform = "rotate(0deg)";
-                // console.log("hii");
-                // after callback (and whole text has been animated), start next text
-                StartTextAnimation(i + 1);
-            });
-        }
+    // text finished, call callback if there is a callback function
+    else if (typeof fnCallback == 'function') {
+      setTimeout(function () {
+        displayCompleteText(fnCallback)
+      }, 10) //after text goes and animation starts
     }
-    // start the text animation
-    StartTextAnimation(0);
+  }
+  // start a typewriter animation for a text in the dataText array
+  function StartTextAnimation(i) {
+    if (typeof dataText[i] == 'undefined') {
+      setTimeout(function () {
+        StartTextAnimation(0);
+      }, 0);
+    }
+    // check if dataText[i] exists
+    if (i < dataText[i].length) {
+      // text exists! start typewriter animation
+      typeWriter(dataText[i], 0, function () {
+        document.getElementById("Rrotate").style.removeProperty("animation");
+        document.getElementById("Rrotate").style.transform = "rotate(0deg)";
+        // console.log("hii");
+        // after callback (and whole text has been animated), start next text
+        StartTextAnimation(i + 1);
+      });
+    }
+  }
+  // start the text animation
+  StartTextAnimation(0);
 });
 
 
@@ -351,41 +372,41 @@ let cubes = document.querySelectorAll(".image-cube");
 let cube = document.querySelectorAll(".image-cube");
 let btnNext = document.querySelector("#next");
 let btnPrev = document.querySelector("#prev");
-function rotateCube(i){
-	cubes[i].style.transform = `rotateY(${pos}deg)`;
+function rotateCube(i) {
+  cubes[i].style.transform = `rotateY(${pos}deg)`;
 }
 let pos = 0;
 
 btnNext.addEventListener("click", () => {
 
   pos -= 90;
-	// for (var i=0;i < cubes.length;i++){
-	// 	setTimeout(rotateCube(i),5000);
-	// }
-	cubes[0].style.transform = `rotateY(${pos}deg)`;
-	console.log(cubes[0].style.transform )
+  // for (var i=0;i < cubes.length;i++){
+  // 	setTimeout(rotateCube(i),5000);
+  // }
+  cubes[0].style.transform = `rotateY(${pos}deg)`;
+  console.log(cubes[0].style.transform)
 
-	setTimeout(()=>{
-		cubes[1].style.transform = `rotateY(${pos}deg)`;
-	},500)
-	setTimeout(()=>{
-		cubes[2].style.transform = `rotateY(${pos}deg)`;
-	},500)
+  setTimeout(() => {
+    cubes[1].style.transform = `rotateY(${pos}deg)`;
+  }, 500)
+  setTimeout(() => {
+    cubes[2].style.transform = `rotateY(${pos}deg)`;
+  }, 500)
 
-	});
+});
 
 
-	btnPrev.addEventListener("click", () => {
-		console.log('hi');
-	  pos += 90;
-		cubes[0].style.transform = `rotateY(${pos}deg)`;
-		setTimeout(()=>{
-			cubes[1].style.transform = `rotateY(${pos}deg)`;
-		},500)
-		setTimeout(()=>{
-			cubes[2].style.transform = `rotateY(${pos}deg)`;
-		},500)
+btnPrev.addEventListener("click", () => {
+  console.log('hi');
+  pos += 90;
+  cubes[0].style.transform = `rotateY(${pos}deg)`;
+  setTimeout(() => {
+    cubes[1].style.transform = `rotateY(${pos}deg)`;
+  }, 500)
+  setTimeout(() => {
+    cubes[2].style.transform = `rotateY(${pos}deg)`;
+  }, 500)
 
-		});
+});
 //Aditya js ends
 
